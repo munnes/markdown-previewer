16edad203c435094ed4c58e699991debb562e251
Nasreen Sayed Siddig Elhussain <nasreen.store@gmail.com>
2019-10-17 05:41:09 +0400

    Updates

---
+++ precache-manifest.712b8048ce91480ac509e3e3093dc1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c467d239304e04dc390be35bd6cd7a5c",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "5fc62f3cd23c4dc8577e",
+    "url": "/markdown-previewer/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "cbd978b1a22e17a73d0b",
+    "url": "/markdown-previewer/static/css/main.74ac773f.chunk.css"
+  },
+  {
+    "revision": "5fc62f3cd23c4dc8577e",
+    "url": "/markdown-previewer/static/js/2.b5299069.chunk.js"
+  },
+  {
+    "revision": "cbd978b1a22e17a73d0b",
+    "url": "/markdown-previewer/static/js/main.c2e6a8d4.chunk.js"
+  },
+  {
+    "revision": "229bd810dd32528e226f",
+    "url": "/markdown-previewer/static/js/runtime-main.05440c54.js"
+  }
+]);
